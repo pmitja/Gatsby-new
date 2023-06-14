30b5c5955e4cdab3460133a84aa78d917c6fea4c
Mitja Pak <mitja.pak@agiledrop.com>
2023-06-14 11:22:21 +0200

    Test

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,60 +1,30 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/how-to/querying-data/use-static-query/
- */
-
 import * as React from "react"
-import { useStaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
-import "./layout.css"
+import PropTypes from "prop-types"
 import { storyblokInit, apiPlugin } from "gatsby-source-storyblok"
-
+import Teaser from "./Teaser/Teaser";
+import Grid from "./Grid/Grid";
+import Feature from "./Feature/Feature";
+ 
 storyblokInit({
   accessToken: process.env.STORYBLOK_ACCESS_TOKEN,
   use: [apiPlugin],
   components: {
-    // components
+    teaser: Teaser,
+    grid: Grid,
+    feature: Feature
   }
 });
-
+ 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: `var(--size-content)`,
-          padding: `var(--size-gutter)`,
-        }}
-      >
-        <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `var(--space-5)`,
-            fontSize: `var(--font-sm)`,
-          }}
-        >
-          © {new Date().getFullYear()} &middot; Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
-    </>
+    <div>
+      <main>{children}</main>
+    </div>
   )
 }
-
+ 
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+ 
 export default Layout
